04cff4bd09dfd2424458e672580f90adc636a182
xBlackEaglex <roberto.smusic.1@gmail.com>
2023-02-01 22:21:52 -0600

    se creo una pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ['placeimg.com', 'api.lorem.space', 'google.pl'],
   },
-}
+};
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  include: ['production'],
+  register: true,
+});
+
+module.exports = withPWA(nextConfig);
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head></Head>
+      <Head>
+        <meta charSet="utf-8"/>
+        <link rel="manifest" href="/manifest.json"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
